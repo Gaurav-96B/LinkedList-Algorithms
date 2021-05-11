2d382264da5c6115946b3f1bc1f63be396488e15
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-11 19:29:22 +0530

    Update Merge Two Sorted Lists.java

--- Sorting Based Linked List Problem/Merge Two Sorted Lists.java
+++ Sorting Based Linked List Problem/Merge Two Sorted Lists.java
@@ -1,1 +1,75 @@
+/* TC=>O(n1+n2)=>O(n)
+SC=>O(n1+n2)*/
 
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head=new ListNode(0);
+        ListNode temp=head;
+        while(l1!=null&&l2!=null)
+        {
+            if(l1.val<=l2.val)
+            {
+                head.next=l1;
+                l1=l1.next;
+                head=head.next;
+            }
+            else
+            {
+                head.next=l2;
+                l2=l2.next;
+                head=head.next;
+            }
+        }
+        if(l1!=null)
+        {
+            head.next=l1;
+        }
+        else
+        {
+            head.next=l2;
+        }
+        return temp.next;
+    }
+}
+
+
+
+
+/*TC=> O(n1+n2)=>O(n)
+SC=>O(1)*/
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1==null)
+        {
+            return l2;
+        }
+        if(l2==null)
+        {
+            return l1;
+        }
+        if(l1.val>l2.val)
+        {
+            ListNode temp=l1;
+            l1=l2;
+            l2=temp;
+        }
+        ListNode res=l1;
+        while(l1!=null&&l2!=null)
+        {
+          ListNode tmp=null;  
+            while(l1!=null && l1.val<=l2.val)
+            {
+               tmp=l1;
+                l1=l1.next;
+            }
+            tmp.next=l2;
+            
+            ListNode temp=l1;
+            l1=l2;
+            l2=temp;
+        }
+        return res;
+    }
+        
+}
