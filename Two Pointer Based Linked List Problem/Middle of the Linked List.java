0e9188fa0d2d4f907669343699ff0a06361c8669
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-11 21:46:37 +0530

    Update Middle of the Linked List.java

--- Two Pointer Based Linked List Problem/Middle of the Linked List.java
+++ Two Pointer Based Linked List Problem/Middle of the Linked List.java
@@ -1,1 +1,32 @@
+/*TC=>O(n)+O(n/2)
+SC=>O(1)
 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp=head;
+        int nodeCount=0;
+        while(temp!=null)
+        {
+          temp=temp.next;
+            nodeCount++;
+           
+        }
+        
+        int traverseNode=(nodeCount/2)+1;
+        ListNode temp1=head;
+        int nodeCount1=1;
+        while(temp1!=null)
+        {
+            if(nodeCount1==traverseNode)
+            {
+                break;
+            }
+            temp1=temp1.next;
+            nodeCount1++;
+             
+            
+        }
+        return temp1;
+        
+    }
+}
