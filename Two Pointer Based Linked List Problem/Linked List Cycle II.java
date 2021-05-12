0f119c795facb5585249391a3810cf135f01a785
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2021-05-12 18:59:46 +0530

    Update Linked List Cycle II.java

--- Two Pointer Based Linked List Problem/Linked List Cycle II.java
+++ Two Pointer Based Linked List Problem/Linked List Cycle II.java
@@ -1,1 +1,22 @@
-
+/*TC=>O(N)
+SC=>O(N)*/
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode>hs=new HashSet<>();
+        ListNode temp=head;
+        while(temp!=null)
+        {
+            if(hs.contains(temp))
+            {
+                return temp;
+            }
+            else
+            {
+                hs.add(temp);
+            }
+            temp=temp.next;
+        }
+        return null;
+        
+    }
+}
