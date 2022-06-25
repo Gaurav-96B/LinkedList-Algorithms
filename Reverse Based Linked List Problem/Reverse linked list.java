a1c0c6716fe7b50cb1f10be99931c86456f2ea27
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2022-06-25 21:13:53 +0530

    Update Reverse linked list.java

--- Reverse Based Linked List Problem/Reverse linked list.java
+++ Reverse Based Linked List Problem/Reverse linked list.java
@@ -1,1 +1,17 @@
-
+Node reverseList(Node head)
+    {
+      if(head==null||head.next==null)
+      {
+          return head;
+      }
+      Node newHead=null;
+      while(head!=null)
+      {
+          Node next=head.next;
+          head.next=newHead;
+          newHead=head;
+          head=next;
+          
+      }
+      return newHead;
+    }
